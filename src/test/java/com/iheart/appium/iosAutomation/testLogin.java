78df3380e71cb9341fface2e592970bd3e1b73d2
Danielle Golinsky <daniellegolinsky@iheartmedia.com>
2016-02-02 14:32:15 -0500

    Added login test to hold login tests from old test files

---
+++ src/test/java/com/iheart/appium/iosAutomation/testLogin.java
@@ -0,0 +1,24 @@
+package com.iheart.appium.iosAutomation;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class testLogin extends TestRoot {
+
+	
+	@Before
+	public void setUp() throws Exception {
+		setup();
+	}
+
+	@After
+	public void after() {
+		TestRoot.tearDown();
+	}
+	
+	@Test
+	public void test_loginViaFacebook() {
+		loginPage.loginViaFacebook();
+	}
+}
