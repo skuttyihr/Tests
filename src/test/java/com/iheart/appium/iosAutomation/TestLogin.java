ef0af1f5443cfce16465838c3d114eb523d62797
Travis Statham <travisStatham@iHeartMedia.com>
2016-07-13 12:20:57 -0400

    Created new ResetPasswordPage, added to TestRoot, created accessibilityIds and methods, added new testcase to TestLogin

--- src/test/java/com/iheart/appium/iosAutomation/TestLogin.java
+++ src/test/java/com/iheart/appium/iosAutomation/TestLogin.java
@@ -51,7 +51,23 @@ public class TestLogin extends TestRoot {
 		LocalTime before = consoleLogStart("Testing IOSElements on Log In Page");
 		loginPage.checkValuesOfElements();
 		consoleLogEnd(before, true, "Tested IOSElements");
-	};
+	}
+	
+	@Test
+	public void testResetPasswordPage(){
+		LocalTime before = consoleLogStart("Testing IOSElements on resetPasswordPage");
+		onboardingPage.clickOnboardingLoginButton();
+		loginPage.clickForgotYourPasswordButton();
+		if(resetPasswordPage.isCurrentlyOnResetPasswordPage() && resetPasswordPage.showAllElements()){
+			resetPasswordPage.clearEmailAddress();
+			resetPasswordPage.enterEmailAddressToResetPassword("abademail@gmail.com");
+			resetPasswordPage.clickResetPasswordButtonWithBadEmail();
+			resetPasswordPage.clickBackButton();
+		}
+		boolean onLoginPage = loginPage.currentlyOnLoginPage();
+		Assert.assertTrue("Not currently on loginPage, check for ResetPassword issues",onLoginPage );
+		consoleLogEnd(before, onLoginPage, "Tested IOSElements on resetPasswordPage");
+	}
 	
 	// Since Google opens Safari, we can't access those elements. 
 	@Ignore
