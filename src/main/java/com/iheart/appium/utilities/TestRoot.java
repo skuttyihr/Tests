0c1df7e90e8075598bdb75d90ed60fcfd48c8643
Danielle Golinsky <daniellegolinsky@iheartmedia.com>
2017-03-21 14:56:40 -0400

    Added retry rule.

--- src/main/java/com/iheart/appium/utilities/TestRoot.java
+++ src/main/java/com/iheart/appium/utilities/TestRoot.java
@@ -18,6 +18,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.rules.MethodRule;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
 import org.openqa.selenium.By;
@@ -1046,6 +1048,59 @@ public class TestRoot{
 
 	}
 
+	/**
+	 * Retries is there is a failure related to a session termination, not an assert or other exception
+	 * @author daniellegolinsky
+	 *
+	 */
+	public class RetryRule implements TestRule{
+		int retries = 1; // default
+		public RetryRule(){
+			this.retries = 1;
+		}
+		public RetryRule(int r){
+			this.retries = r;
+		}
+		
+		@Override
+		public Statement apply(Statement base, Description description) {
+			return statement(base, description);
+		}
+		
+		private Statement statement(final Statement base, final Description description){
+			return new Statement(){
+				@Override
+				public void evaluate() throws Throwable {
+					// Save the exception for the end of the retries
+					Throwable caughtThrowable = null;
+					
+					for (int i = 0; i < retries; i++){
+						try{
+							base.evaluate();
+							return; // End
+						}
+						catch (Throwable t){
+							System.out.println(t);
+							caughtThrowable = t;
+							// Only retry if it was a driver issue, not an assertion
+							if(!(t instanceof java.lang.AssertionError)
+									&& t.getMessage().contains("session is either terminated or not started")){
+								System.err.println("\n\nRun #" + (i + 1) + " failed, may retry.");
+							}
+							else{
+								throw t;
+							}
+						}
+					}
+					if (caughtThrowable != null){
+						throw caughtThrowable;
+					}
+				}
+			};
+		}
+	}
+	
+	
 	/**
 	 * Screenshot Rule Runs after every test. If the test passed, this runs the
 	 * usual shutdown method If it failed, it takes a screenshot, then runs the
